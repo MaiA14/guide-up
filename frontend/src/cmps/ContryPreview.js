d42af0ce6cd2b1b0094afb7bf3202699f38973f1
ben yeger <benyager36@gmail.com>
2020-01-16 19:08:17 +0200

    ben 19:08

--- frontend/src/cmps/ContryPreview.js
+++ frontend/src/cmps/ContryPreview.js
@@ -9,7 +9,7 @@ export default class ContryPreview extends Component {
         const { props } = this
         return (
          
-                <Link className={props.country} to={`/location/?city=${props.country}`}>
+                <Link  className={props.country} to={`/location/?city=${props.country}`}>
                     <div>
                         <div className="country-preview-card">{props.country}</div>
                     </div></Link>
