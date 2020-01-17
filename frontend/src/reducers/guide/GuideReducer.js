1ceb49c22436c3d299350aafc54fac12101197b2
Mai Aloni <maiaa1993@gmail.com>
2020-01-17 23:18:09 +0200

    update

--- frontend/src/reducers/guide/GuideReducer.js
+++ frontend/src/reducers/guide/GuideReducer.js
@@ -12,6 +12,7 @@ const guides= [
     { name: 'Kristina Bazan', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/g5ia1n6.png'},
     { name: 'Donna Smith', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/VxGl3q0.png'},
     { name: 'Andy Torres', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/0KKU0VA.png'},
+    { name: 'Jordan Clarke', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/V1KvTQX.jpg'},
     { name: 'Melissa Alatorre', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/YUfx8Zs.png'}
 ]
 
