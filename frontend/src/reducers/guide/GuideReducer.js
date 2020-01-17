c5b427ae3795766b3cc9bfe57803713ddc39e166
Mai Aloni <maiaa1993@gmail.com>
2020-01-17 23:41:00 +0200

    update

--- frontend/src/reducers/guide/GuideReducer.js
+++ frontend/src/reducers/guide/GuideReducer.js
@@ -2,17 +2,22 @@ const guides= [
     { name: 'Lihi Gozlan', description:'about me', langugages:'Hebrew, English', _id: '123',city:'tel-aviv', imgUrl: 'https://i.imgur.com/UA5CUkB.png'},
     { name: 'Ron Levy', description:'about me', langugages:'Hebrew, English', _id: '123',city:'tel-aviv', imgUrl: 'https://i.imgur.com/PjT8rth.png'},
     { name: 'Shani Sharon', description:'about me', langugages:'Hebrew, English, Spanish', _id: '123',city:'tel-aviv', imgUrl: 'https://i.imgur.com/F2v7Wua.png'},
+
     { name: 'Michèle Krüsi', description:'about me', langugages:'French,Swedish',_id: '321',city:'paris', imgUrl: 'https://i.imgur.com/IvVU9Z1.png'},
     { name: 'Philip montre', description:'about me', langugages:'French,Swedish, Italian',_id: '321',city:'paris', imgUrl: 'https://i.imgur.com/UyDensn.png'},
     { name: 'Francesca Hayward', description:'about me', langugages:'French,Swedish, Russian',_id: '321',city:'paris', imgUrl: 'https://i.imgur.com/rGll9yJ.png'},
     { name: 'Sara Sanchezss', description:'about me', langugages:'French,Swedish',_id: '321',city:'paris', imgUrl: 'https://i.imgur.com/scq2FiT.png'},
+
     { name: 'Aida Domenech',description:'about me', langugages:'Spanish, Italian, French',_id: '323',city:'barcelona', imgUrl: 'https://i.imgur.com/3SYbClk.png'},
     { name: 'Pablo Habrez',description:'about me', langugages:'Spanish, Italian',_id: '323',city:'barcelona', imgUrl: 'https://i.imgur.com/bWsYMbG.png'},
     { name: 'Rodrirogo Labes',description:'about me', langugages:'Spanish, Italian',_id: '323',city:'barcelona', imgUrl: 'https://i.imgur.com/oqQN5vY.png'},
+    { name: 'Jolyn Cyrnn',description:'about me', langugages:'Spanish, Italian',_id: '323',city:'barcelona', imgUrl: 'https://i.imgur.com/COSWBgK.png'},
+
     { name: 'Looise Cooney', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/SXXdCG4.jpg3'},
     { name: 'Kristina Bazan', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/g5ia1n6.png'},
     { name: 'Donna Smith', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/VxGl3q0.png'},
     { name: 'Brianna Williams', description:'about me', langugages:'English, Spanish', _id: '623',city:'new-york', imgUrl: 'https://i.imgur.com/G8frw5M.png'},
+
     { name: 'Andy Torres', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/0KKU0VA.png'},
     { name: 'Jordan Clarke', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/V1KvTQX.jpg'},
     { name: 'Melissa Alatorre', description:'about me', langugages:'English, Spanish',_id: '654',city:'mexico', imgUrl: 'https://i.imgur.com/YUfx8Zs.png'}
