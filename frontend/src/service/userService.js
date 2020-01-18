89892f9c1caf04eb603a374caa99b5d0eb6ce78c
Mai Aloni <maiaa1993@gmail.com>
2020-01-18 23:17:23 +0200

    new guides

---
+++ frontend/src/service/userService.js
@@ -0,0 +1,45 @@
+import HttpService from './HttpService.js'
+import axios from 'axios';
+export default {
+    query,
+    deleteUser,
+    addUser,
+    getUserById,
+    editUser,
+};
+const url = "http://localhost:3001/api/user"
+var Axios = axios.create({
+    withCredentials: true
+});
+
+
+function query(filterBy = '') {
+    return Axios.get(url).then(res => res.data)
+}
+
+
+function getUserById(userId) {
+    return HttpService.get(`user/${userId}`)
+
+}
+
+
+function editUser(user,userId) {
+    return HttpService.put(`user${userId}`, user).then(res => res.data);
+}
+
+function addUser(name, user) {
+    return HttpService.post('user', user).then(res => res.data);
+}
+
+function deleteUser(userId) {
+    return HttpService.delete(`user${userId}`).then(res => res.data);
+}
+
+
+
+
+// function getguideById(guideId){
+//     return  axios.get('/guides/' +guideId)
+// }
+
