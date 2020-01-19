1116bce8521ad081306f683cc7374b75388ec6f5
Mai Aloni <maiaa1993@gmail.com>
2020-01-19 15:11:17 +0200

    added login

--- frontend/src/cmps/Experience.js
+++ frontend/src/cmps/Experience.js
@@ -5,6 +5,10 @@ export default class Experience extends Component {
         return (
             <div>
                 <h1>Tell us about yourself</h1>
+                <div>Write a short description about yourself</div>
+                <div><input type="text" className="experience-input"></input></div>
+                <div>Add a profile picture</div>
+                <div>Add photos that describes you best</div>
             </div>
         )
     }
