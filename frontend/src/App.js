67c68476c3d8851c1012d8e16df789a168c61369
Mai Aloni <maiaa1993@gmail.com>
2020-01-20 15:31:08 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,13 +25,12 @@ function App() {
         <Router>
       <Navbar></Navbar>       
           <Switch>
-            <Route component={LocalGuideApp} path="/home" exact></Route>
- 
+            <Route component={LocalGuideApp} path="/" exact></Route>
             <Route component={GuideDetails} path="/guides" exact></Route>
             <Route component={FilteredGuideList} path="/location" exact></Route>
-            <Route component={Login} path="/login" exact></Route>
-            <Route component={Signup} path="/signup" exact></Route>
-            <Route component={BecomeGuide} path="/join_us"></Route>
+            <Route component={Login} path="/login" ></Route>
+            <Route component={Signup} path="/signup" ></Route>
+            <Route component={BecomeGuide} path="/join_us" ></Route>
 
           {/* <Route component={Signup} path="/signup" exact></Route>
           <Route component={AddGuide} path="/addGuide" exact></Route>  */}
--- frontend/src/cmps/Navbar.js
+++ frontend/src/cmps/Navbar.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Navbar(props) {
 
     return <div className="main-navbar">
-        <Link to="/home" > <div className="flex logo">
+        <Link to="/" > <div className="flex logo">
 
             <img src="https://i.ibb.co/8BxPMdT/logo.png" alt="logo"></img>
             <div className="logo-text">Local Guide</div>
@@ -14,9 +14,9 @@ export default function Navbar(props) {
          
         </div>
         <div className=" nav-links">
-            <div className="nav-link"><NavLink activeClassName="active" to='/home'>Home</NavLink></div>
-            <div className="nav-link"><NavLink activeClassName="active" to='/login'>Login</NavLink></div>
-            <div className="nav-link"><NavLink activeClassName="active" to='/join_us'>Become a guide</NavLink></div>
+            <div className="nav-link"><NavLink activeClassName="active" to='/' exact>Home</NavLink></div>
+            <div className="nav-link"><NavLink activeClassName="active" to='/login' exact>Login</NavLink></div>
+            <div className="nav-link"><NavLink activeClassName="active" to='/join_us' exact>Become a guide</NavLink></div>
         </div>
     </div>
 
