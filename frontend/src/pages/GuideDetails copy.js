e7fc0998bae56443acefd621c427577c950c8de7
Mai Aloni <maiaa1993@gmail.com>
2020-01-25 02:52:18 +0200

    mai 02:52

--- frontend/src/pages/GuideDetails copy.js
+++ frontend/src/pages/GuideDetails copy.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react'
 import quoryString from 'query-string'
 import { connect } from 'react-redux'
 import { Rating } from 'semantic-ui-react'
-
-
 import Calendar from '../cmps/Calendar.js'
 import { getGuide } from '../reducers/guide/actionGuide.js'
 import Review from '../cmps/Review.js'
 import Navbar from '../cmps/Navbar.js'
-
 
 class GuideDetails extends Component {
     state = {
@@ -25,21 +22,14 @@ class GuideDetails extends Component {
             } else {
                 bookClassName = 'fixed'
                 this.setState({ bookClassName })
-
             }
-
         }
-
-
         const items = quoryString.parse(this.props.location.search)
         this.props.getGuide(items.guide_id);
-
-
     }
+
     backToListOfGuids = () => {
-
         this.props.history.goBack()
-
     }
 
     onNewReview = (ev) => {
@@ -60,42 +50,37 @@ class GuideDetails extends Component {
                             <div className="btn-container">
                                 <button onClick={this.backToListOfGuids} className="back-btn">Back</button>
                             </div>
-                            <div className="guide-header ">
-                                <h1>{this.props.guide.name}</h1>
-                            </div>
+                            <h1 className="guide-header ">
+                                {this.props.guide.name}
+                            </h1>
                             <div className="guide-img-details">
                                 <img src={this.props.guide.imgUrl} ></img>
                             </div>
-                            <div className="guide-desc">
+                          
+                            <div className="guide-short-desc">
                                 <h2>{this.props.guide.shortDescription}</h2>
                             </div>
-                            <div className="From">
-                                <span> From: {this.props.guide.city}</span>
+                            <div class="lang-from-guide">
+                            <div className="guide-from">
+                                <div> From: {this.props.guide.city}</div>
                             </div>
-                            <div className="Langugages">
-                                <span>Langugages: {this.props.guide.langugages}</span>
+    
+                            <div className="guide-lang">
+                                <div>Langugages: {this.props.guide.langugages}</div>
                             </div>
-
+                            </div>
+                    
                             <div className="contanier-Details">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                     Vestibulum vitae pulvinar felis,
-                                      in sagittis libero. Donec at libero pulvinar,
-                                       condimentum ex in, commodo quam. Nam et mollis nisl.
-                                        Phasellus fringilla libero tortor,
-                                        semper ultrices purus suscipit vel.
-                                        Pellentesque in metus quis metus egestas blandit
-                                         a sit amet purus. Vestibulum ante ipsum primis in
-                                         faucibus orci luctus et ultrices posuere cubilia Curae;
-                                         Maecenas rhoncus turpis porta rutrum pharetra. Fusce consectetur
-                                          malesuada volutpat. Cras volutpat nunc libero, sed fermentum
-                                          urna egestas congue. In vitae sapien mattis, vehicula lectus non,
-                                           eleifend quam. Aliquam vitae enim eget felis faucibus dictum.</p>
+                    
+                                <div>{this.props.guide.description}</div>
+                                <Review guide={this.onNewReview} ></Review>
+                                
                             </div>
                         </div>
                         <div className="calendar-container">
                             <div className="calendar-content">
                                 <Calendar></Calendar>
-                                <div className="HowMany-people ">
+                                <div className="HowMany-people">
                                     <h2>How many people?</h2>
                                 </div>
                                 <select></select>
