66d6e90efcea844ceedb5e394a095aca2b332cee
Mai Aloni <maiaa1993@gmail.com>
2020-02-01 23:15:36 +0200

    mai - chat 23:15

--- frontend/src/cmps/Messenger.js
+++ frontend/src/cmps/Messenger.js
@@ -1,34 +1,30 @@
 import React from 'react'
-import Calendar from '../cmps/Calendar.js'
+import Chatbox from '../cmps/Chatbox.js'
 
 function Messenger(props) {
     return (
         <div className="">
             <div className="chat-container">
-
                 <div className="chat-list flex column">
                     <div className="chat-title">Chat</div>
                     <div className="flex">
                         <div className="chat-messages">
                             <div className="chat-details">
-                                <img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1580585567/liivrwjfvseuovfquyxp.png" className="chat-user-img"></img>
+                                <img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1580585567/liivrwjfvseuovfquyxp.png" 
+                                className="chat-user-img"></img>
                                 <div className="chat-guide-name">Lihi Cohen</div>
                             </div>
                         </div>
                         <div className="chat-box">
+                            <Chatbox></Chatbox>
                         </div>
                     </div>
-
                 </div>
-
                 <div className="chat-header">
-
                 </div>
-
             </div>
         </div >
     )
 }
 
 export default Messenger
-
