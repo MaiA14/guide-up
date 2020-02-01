bd7133bf38ea323a378d40f25e00e38dfdf4ca8f
Mai Aloni <maiaa1993@gmail.com>
2020-02-02 01:23:40 +0200

    mai - fixed chat  01:23

--- frontend/src/cmps/Chatbox.js
+++ frontend/src/cmps/Chatbox.js
@@ -5,23 +5,23 @@ export default class Chatbox extends Component {
     render() {
         return (
             <div>
-               <div className="chat-messages-chatbox">
-                            <div className="chat-details">
-                                <img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1580585567/liivrwjfvseuovfquyxp.png" 
-                                className="chat-user-img"></img>
-                                <div className="chat-guide-name">Lihi Cohen</div>
-                            </div>
-                            <div className="chat-window">
-                            <div className="bubble-message">message</div>
-                            <div className="bubble-message">message</div>
-                            <div className="bubble-message">message</div>
-                            </div>
-                            <div className="chat-textarea-send-btn">
-                            <TextArea className="chat-textarea" placeholder='Type a message...' />
-                            <button className="send-btn">Send</button>
-                            </div>
-                            
-                  </div>   
+                <div className="chat-messages-chatbox">
+                    <div className="chat-details">
+                        <img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1580585567/liivrwjfvseuovfquyxp.png"
+                            className="chat-user-img"></img>
+                        <div className="chat-guide-name">Lihi Cohen</div>
+                    </div>
+                    <div className="chat-window">
+                        <div className="bubble-message">message</div>
+                        <div className="bubble-message">message</div>
+                        <div className="bubble-message">message</div>
+                    </div>
+                    <div className="message-container">
+                        <input type="text" className="message-input" placeholder="Type a message..."></input>
+                        <button className="send-btn">Send</button>
+                    </div>
+
+                </div>
             </div>
         )
     }
