34eccd6f6128adcc4d53f0d4c32a4767f5227546
ben y <benyager36@gmail.com>
2020-03-17 11:49:11 +0200

    ben 11:48

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import thunk from 'redux-thunk';
 
 import LocalGuideApp from './pages/LocalGuideApp.js'
 import GuideDetails from '../src/pages/GuideDetails.js';
